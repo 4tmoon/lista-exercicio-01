f94f9b7410b7be68ac69a374e475f180f3152ce3
Anaí Rojas <rm559049@fiap.com.br>
2025-04-03 22:20:20 -0300

    enviando exercícios 06 e 07

---
+++ src/exercicio06/Main.java
@@ -0,0 +1,13 @@
+package exercicio06;
+
+public class Main {
+    public static void main(String[] args) {
+        Hora h1 = new Hora(3,30,0);
+        Hora h2 = new Hora(23,45,02);
+        Hora h3 = new Hora(48, 125, 963);
+
+        System.out.println(h1.validar());
+        System.out.println(h2.validar());
+        System.out.println(h3.validar());
+    }
+}
---
+++ src/exercicio07/Bola.java
@@ -0,0 +1,31 @@
+package exercicio07;
+
+public class Bola {
+    private String cor;
+    private double raio;
+
+    public Bola(String cor, double raio) {
+        this.cor = cor;
+        this.raio = raio;
+    }
+
+    public Bola maiorBola(Bola b1, Bola b2){
+        Bola aux = null;
+
+        if (this.raio > b1.raio && this.raio > b2.raio){
+            aux = this;
+        }
+        else if(b1.raio > b2.raio) {
+            aux = b1;
+        } else {
+            aux = b2;
+        }
+        return aux;
+    }
+
+    public String retornarDados(){
+        return cor + " " + raio;
+    }
+
+
+}
