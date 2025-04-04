b5cbfa06100cfb5e01240ea626b618cd84407b3c
Anaí Rojas <rm559049@fiap.com.br>
2025-04-03 22:37:30 -0300

    enviando  o exercício 03

---
+++ src/exercicio03/Main.java
@@ -0,0 +1,10 @@
+package exercicio03;
+
+public class Main {
+    public static void main(String[] args) {
+        Retangulo r1 = new Retangulo(4.3, 9);
+
+        System.out.println(r1.calcularArea());
+        System.out.println(r1.calcularPerimetro());
+    }
+}
